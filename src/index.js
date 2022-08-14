6b951c0b188d165786bf736808cdd7ea136df390
Vivek_Gupta <vivekgupta86988@gmail.com>
2022-08-14 11:20:09 +0530

    testin reatjs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import React from "react";
+import App from "./App"
+// import App1 from "./App1"
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <Home />,
+     <App />,
+     <App1 />  
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Home() {
+  return (
+    <div>
+      <h1>this is first pages</h1>
+    </div>
+  );
+}
+
+function App1(){
+  return (
+      <>
+      <h1>helli vivek this is another component</h1>
+      <h4>first you have to create component then you import  in index.js those file are direct with index.html</h4>
+      </>
+
+  )
+}
